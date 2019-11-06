c9a00de2015729650e68598ea2e4b72fca89d8a8
hpstream <915414300@qq.com>
2019-11-06 17:44:05 +0800

    fix: 提交初始化代码

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+// interface MVVM {};
+
+
+// function MVVM(){
+
+// }
+
+// new MVVM();
+
